67a6938b5b7e23c797fdcda88183fd2d6bdeb9ca
nskrai <matsushita@ngc-ngc.jp>
2018-05-24 17:12:40 +0900

    syokyu1-1

--- db/migrate/20180524062819_remove_checkbox_from_posts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCheckboxFromPosts < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :posts, :checkbox, :string
-    remove_column :posts, :boolean, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_025158) do
+ActiveRecord::Schema.define(version: 2018_05_24_071706) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_025158) do
     t.datetime "updated_at", null: false
     t.boolean "checkbox"
     t.boolean "ispublish"
+    t.string "status"
   end
 
 end
